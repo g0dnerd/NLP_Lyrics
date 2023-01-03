8436b129c7827ad7e2bcbd33999f258c39d5bf25
Paul Kukowski <kukowskipaul@gmail.com>
2023-01-04 00:43:15 +0100

    Further refined markov chains algorithm and added some debugging output

--- main.py
+++ main.py
@@ -17,13 +17,16 @@ if __name__ == "__main__":
     geniusParser = genius_parser._GeniusParser()
     artist_id = geniusParser.get_artist_id({args.artist})
     songs = geniusParser.get_songs(artist_id)
+    print("Parsed " + str(len(songs)) + " songs by " + str(args.artist))
     lyrics = ""
-    print("Scraping lyrics from html")
 
+    song_counter = 1
     for song in songs:
         url = geniusParser.get_song_url(
             song['primary_artist']['name'], song['title'])
         lyrics += geniusParser.get_lyrics(url)
+        print('Scraping lyrics from song %d out of %d' %(song_counter, len(songs)), end='\r')
+        song_counter += 1
 
     with open('lyrics.txt', 'w') as f:
         f.write(lyrics)
@@ -35,6 +38,6 @@ if __name__ == "__main__":
 
     elif {args.mode} == {'markov'}:
         markovGenerator = markov_generation.MarkovGenerator()
-        new_lyrics = markovGenerator.generate_lyrics(lyrics)
+        new_lyrics = markovGenerator.generate_lyrics(lyrics, 10, 10)
 
 print(new_lyrics)
--- markov_generation.py
+++ markov_generation.py
@@ -4,15 +4,39 @@ import random
 
 class MarkovGenerator:
 
-    def generate_lyrics(self, corpus):
+    # def generate_lyrics(self, corpus):
+        # # Split the corpus into a list of words
+        # words = re.findall(r'\w+', corpus)
+# 
+        # with open('words.txt', 'w') as f:
+            # for word in words:
+                # f.write(word)
+                # f.write("\n")
+        # print("Successfully parsed dictionary of " + str(len(words)) + " words")
+# 
+        # # Create a dictionary that maps words to lists of words that follow them
+        # markov_chain = {}
+        # for i, word in enumerate(words[:-1]):
+            # if word not in markov_chain:
+                # markov_chain[word] = []
+            # markov_chain[word].append(words[i + 1])
+# 
+        # # Generate random lyrics
+        # lyrics = ""
+        # for i in range(10):
+            # word = random.choice(list(markov_chain.keys()))
+            # line = [word.capitalize()]
+            # num_words = 1
+            # while word in markov_chain and num_words < 10:
+                # word = random.choice(markov_chain[word])
+                # line.append(word)
+                # num_words += 1
+            # lyrics += " ".join(line) + "\n"
+        # return lyrics
+
+    def generate_lyrics(self, corpus, max_lines, max_words_per_line):
         # Split the corpus into a list of words
         words = re.findall(r'\w+', corpus)
-
-        with open('words.txt', 'w') as f:
-            for word in words:
-                f.write(word)
-                f.write("\n")
-        print("Successfully parsed dictionary of " + str(len(words)) + " words")
 
         # Create a dictionary that maps words to lists of words that follow them
         markov_chain = {}
@@ -23,13 +47,26 @@ class MarkovGenerator:
 
         # Generate random lyrics
         lyrics = ""
-        for i in range(10):
-            word = random.choice(list(markov_chain.keys()))
-            line = [word.capitalize()]
-            num_words = 1
-            while word in markov_chain and num_words < 10:
-                word = random.choice(markov_chain[word])
-                line.append(word)
+        line_number = 1
+        word = random.choice(list(markov_chain.keys()))
+        line = word.capitalize()
+        num_words = 1
+        while line_number <= max_lines:
+            if word in markov_chain and num_words < max_words_per_line:
+                # Choose the next word according to the probabilities in the markov chain
+                next_word_probs = markov_chain[word]
+                weights = [next_word_probs.count(w) for w in next_word_probs]
+                next_word = random.choices(next_word_probs, weights=weights)[0]
+                line += ' ' + next_word
+                word = next_word
                 num_words += 1
-            lyrics += " ".join(line) + "\n"
+            else:
+                # End the line and start a new one
+                lyrics += line + "\n"
+                line_number += 1
+                if line_number > max_lines:
+                    break
+                word = random.choice(list(markov_chain.keys()))
+                line = word.capitalize()
+                num_words = 1
         return lyrics
