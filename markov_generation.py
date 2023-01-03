7b7b451da4df5fbdfcfb94f0f66592315fb45682
Paul Kukowski <kukowskipaul@gmail.com>
2023-01-04 00:43:41 +0100

    Further refined markov chains algorithm and added some debugging output

--- markov_generation.py
+++ markov_generation.py
@@ -4,36 +4,6 @@ import random
 
 class MarkovGenerator:
 
-    # def generate_lyrics(self, corpus):
-        # # Split the corpus into a list of words
-        # words = re.findall(r'\w+', corpus)
-# 
-        # with open('words.txt', 'w') as f:
-            # for word in words:
-                # f.write(word)
-                # f.write("\n")
-        # print("Successfully parsed dictionary of " + str(len(words)) + " words")
-# 
-        # # Create a dictionary that maps words to lists of words that follow them
-        # markov_chain = {}
-        # for i, word in enumerate(words[:-1]):
-            # if word not in markov_chain:
-                # markov_chain[word] = []
-            # markov_chain[word].append(words[i + 1])
-# 
-        # # Generate random lyrics
-        # lyrics = ""
-        # for i in range(10):
-            # word = random.choice(list(markov_chain.keys()))
-            # line = [word.capitalize()]
-            # num_words = 1
-            # while word in markov_chain and num_words < 10:
-                # word = random.choice(markov_chain[word])
-                # line.append(word)
-                # num_words += 1
-            # lyrics += " ".join(line) + "\n"
-        # return lyrics
-
     def generate_lyrics(self, corpus, max_lines, max_words_per_line):
         # Split the corpus into a list of words
         words = re.findall(r'\w+', corpus)
